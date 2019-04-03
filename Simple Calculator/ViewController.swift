5fe908c472bf8643b48722af37eddbd82c99e981
Peter Jenkin <peternjenkin@gmail.com>
2019-04-03 19:42:44 +0100

    3-22 basic calculator buttons working

--- Simple Calculator/ViewController.swift
+++ Simple Calculator/ViewController.swift
@@ -9,36 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var firstNumberTxt: UITextField!
+    @IBOutlet weak var secondNumberTxt: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
 
+    var result = 0
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        resultLabel?.text = "Result is:  "
+        
     }
-    @IBOutlet weak var firstNumberTxt: UITextField!
+    
+    
 
-    @IBOutlet weak var secondNumberTxt: UITextField!
     
-    
-    @IBOutlet weak var resultLabel: UILabel!
-    
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    //override func didReceiveMemoryWarning() {
+        //super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    //}
 
     @IBAction func plusBtn(_ sender: Any) {
+        result = Int(firstNumberTxt.text!)! + Int(secondNumberTxt.text!)!
+        // implicitly unwrapped optional - need to be certain that values will exist
+        resultLabel.text = String(result)
     }
     
     @IBAction func minusBtn(_ sender: Any) {
+        result = Int(firstNumberTxt.text!)! - Int(secondNumberTxt.text!)!
+        resultLabel.text = String(result)
     }
 
     @IBAction func divideBtn(_ sender: Any) {
+        result = Int(firstNumberTxt.text!)! / Int(secondNumberTxt.text!)!
+        resultLabel.text = String(result)
     }
     @IBAction func mutltiplyBtn(_ sender: Any) {
+        result = Int(firstNumberTxt.text!)! * Int(secondNumberTxt.text!)!
+        resultLabel.text = String(result)
     }
+    
     @IBAction func remainderBtn(_ sender: Any) {
+        result = Int(firstNumberTxt.text!)! % Int(secondNumberTxt.text!)!
+        resultLabel.text = String(result)
     }
 }
 
